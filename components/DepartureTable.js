a0ba45b769fe7211bd4d2ac0000db269db632e3e
Nico Niebergall <nico.niebergall@artif.com>
2019-02-08 13:56:52 +0100

    Changed: Added some table-ness via @rebass/grid. Still deciding on implementation details for the design system.

---
+++ components/DepartureTable.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Flex, Box } from '@rebass/grid';
+import { distanceInWordsToNow } from 'date-fns';
+import deLocale from 'date-fns/locale/de';
+
+import { rhythm } from '../utilities/styles';
+
+const DepartureTable = ({ departureData }) => (
+  <React.Fragment>
+    <Flex flexDirection="column">
+      <Flex css={`border-bottom: 1px solid`} flexWrap="wrap">
+        <Box width={1 / 3}>
+          Linie
+        </Box>
+        <Box width={1 / 3}>
+          Richtung
+        </Box>
+        <Box width={1 / 3}>
+          Abfahrt
+        </Box>
+      </Flex>
+      <React.Fragment>
+        {departureData.map((oneDeparture, index) => (
+          <Flex css={`
+            border-bottom: 1px solid;
+          `} key={oneDeparture.id}>
+            <Box width={1 / 3}>
+              {oneDeparture.line}
+            </Box>
+            <Box width={1 / 3}>
+              {oneDeparture.direction}
+            </Box>
+            <Box width={1 / 3}>
+              {distanceInWordsToNow(oneDeparture.departure, { locale: deLocale })}
+            </Box>
+          </Flex>
+        ))}
+      </React.Fragment>
+    </Flex>
+  </React.Fragment>
+);
+
+DepartureTable.propTypes = {
+  departureData: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string,
+      line: PropTypes.number,
+      direction: PropTypes.string,
+      departure: PropTypes.number,
+    }).isRequired,
+  ),
+};
+
+export default DepartureTable;
--- pages/station.js
+++ pages/station.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
 import { addMinutes, getTime } from 'date-fns';
 import { generate as shortid } from 'shortid';
@@ -6,28 +7,46 @@ import Head from '../components/Head';
 
 import Header from '../components/Header';
 import Container from '../components/Container';
-import DepartureTable from '../components/DepartureTable/index';
+import DepartureTable from '../components/DepartureTable';
 
 import { getStationById, getStations } from '../data/stations';
 
 const mockDepartureData = [
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
   {
-    id: shortid(), line: Math.floor(Math.random() * 10) + 1, direction: 'Beispiel', departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
+    id: shortid(),
+    line: Math.floor(Math.random() * 10) + 1,
+    direction: 'Beispiel',
+    departure: getTime(addMinutes(new Date(), Math.floor(Math.random() * 30) + 1)),
   },
 ];
 
@@ -37,10 +56,12 @@ const Station = ({ router: { query: { id } } }) => (
     <Header>
       {getStationById(id).stop}
     </Header>
-    <Container>
-      <DepartureTable departureData={mockDepartureData} />
-    </Container>
+    <DepartureTable departureData={mockDepartureData} />
   </div>
 );
+
+Station.propTypes = {
+  router: PropTypes.object.isRequired,
+};
 
 export default withRouter(Station);
